8969306d4c058097183a22ff30cb47f01372e984
Damien Couchez <damien.couchez@gmail.com>
2021-02-18 10:45:48 +0100

    Adding credentials check to prevent Magento from crashing on fresh install (#1125)

--- Model/Indexer/PageObserver.php
+++ Model/Indexer/PageObserver.php
@@ -2,6 +2,7 @@
 
 namespace Algolia\AlgoliaSearch\Model\Indexer;
 
+use Algolia\AlgoliaSearch\Helper\ConfigHelper;
 use Magento\Framework\Indexer\IndexerRegistry;
 use Magento\Framework\Model\AbstractModel;
 
@@ -9,15 +10,29 @@ class PageObserver
 {
     private $indexer;
 
-    public function __construct(IndexerRegistry $indexerRegistry)
-    {
+    /**
+     * @var ConfigHelper
+     */
+    private $configHelper;
+
+    public function __construct(
+        IndexerRegistry $indexerRegistry,
+        ConfigHelper $configHelper
+    ) {
         $this->indexer = $indexerRegistry->get('algolia_pages');
+        $this->configHelper = $configHelper;
     }
 
     public function beforeSave(
         \Magento\Cms\Model\ResourceModel\Page $pageResource,
         AbstractModel $page
     ) {
+        if (!$this->configHelper->getApplicationID()
+            || !$this->configHelper->getAPIKey()
+            || !$this->configHelper->getSearchOnlyAPIKey()) {
+            return [$page];
+        }
+
         $pageResource->addCommitCallback(function () use ($page) {
             if (!$this->indexer->isScheduled()) {
                 $this->indexer->reindexRow($page->getId());
@@ -31,6 +46,12 @@ class PageObserver
         \Magento\Cms\Model\ResourceModel\Page $pageResource,
         AbstractModel $page
     ) {
+        if (!$this->configHelper->getApplicationID()
+            || !$this->configHelper->getAPIKey()
+            || !$this->configHelper->getSearchOnlyAPIKey()) {
+            return [$page];
+        }
+
         $pageResource->addCommitCallback(function () use ($page) {
             if (!$this->indexer->isScheduled()) {
                 $this->indexer->reindexRow($page->getId());
