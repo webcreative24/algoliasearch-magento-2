6a9ebd2b49707dd7f5d1d4384ecd7b5db2e02b9f
Betty Suravech <bsuravech@gmail.com>
2021-03-05 10:51:47 +0100

    Update module sequence and indexers (#1132)

--- Model/Indexer/Product.php
+++ Model/Indexer/Product.php
@@ -9,12 +9,11 @@ use Algolia\AlgoliaSearch\Helper\Entity\ProductHelper;
 use Algolia\AlgoliaSearch\Model\IndexMover;
 use Algolia\AlgoliaSearch\Model\IndicesConfigurator;
 use Algolia\AlgoliaSearch\Model\Queue;
-use Magento;
 use Magento\Framework\Message\ManagerInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
-class Product implements Magento\Framework\Indexer\ActionInterface, Magento\Framework\Mview\ActionInterface
+class Product implements \Magento\Framework\Indexer\ActionInterface, \Magento\Framework\Mview\ActionInterface
 {
     private $storeManager;
     private $productHelper;
