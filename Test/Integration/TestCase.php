8474e54abfc178ef8fb4d47336341b8de6ddeb93
Betty Suravech <bsuravech@gmail.com>
2021-03-29 21:29:23 +0200

    Update CI testing (#1140)

--- Test/Integration/TestCase.php
+++ Test/Integration/TestCase.php
@@ -114,10 +114,10 @@ abstract class TestCase extends \TC
         $this->configHelper = $config = $this->getObjectManager()->create(ConfigHelper::class);
 
         $this->setConfig('algoliasearch_credentials/credentials/application_id', getenv('ALGOLIA_APPLICATION_ID'));
-        $this->setConfig('algoliasearch_credentials/credentials/search_only_api_key', getenv('ALGOLIA_SEARCH_API_KEY'));
+        $this->setConfig('algoliasearch_credentials/credentials/search_only_api_key', getenv('ALGOLIA_SEARCH_KEY_1') ?: getenv('ALGOLIA_SEARCH_API_KEY'));
         $this->setConfig('algoliasearch_credentials/credentials/api_key', getenv('ALGOLIA_API_KEY'));
 
-        $this->indexPrefix =  'TRAVIS_M2_' . getmypid() . (getenv('INDEX_PREFIX') ?: 'magento20tests_');
+        $this->indexPrefix =  'magento2_' . date('Y-m-d_H:i:s') . '_' . (getenv('INDEX_PREFIX') ?: 'circleci_');
         $this->setConfig('algoliasearch_credentials/credentials/index_prefix', $this->indexPrefix);
 
         $this->boostrapped = true;
